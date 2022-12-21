e31f28bcb087c6a9d6797b783d390a88915852fb
Ahmedmahmoued9 <120914476+Ahmedmahmoued9@users.noreply.github.com>
2022-12-21 15:59:29 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -20,17 +20,17 @@ namespace Task_Discrete_Mathematics_2
             // first loop to determine the start and end number taken from user
             for (int number = start; number <= end; number++)
             {
-                // seconed to determine the divider start and end 
-                for (int divider = 1; divider <= end; divider++)
+                // seconed to determine the Divisor start and end 
+                for (int Divisor = 1; Divisor <= end; Divisor++)
                 {
-                    // first condition to make sure that "number" can be divided on the "divider" and "number" don't equal "divider" 
-                    if (number % divider==0 && number != divider)
+                    // first condition to make sure that "number" can be divided on the "Divisor" and "number" don't equal "Divisor" 
+                    if (number % Divisor == 0 && number != Divisor)
                     {
-                        // here we add the dividers 
-                        sum += divider;
+                        // here we add the Divisor 
+                        sum += Divisor;
                     }
                 }
-                //seconed condition to make sure the sum of dividers equal the givein number
+                //seconed condition to make sure the sum of Divisor equal the given number
                 if (sum == number)
                 {
                     Console.WriteLine("is perfect number : "+number);
